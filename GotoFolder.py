da7e467a71a3eb411d0e524febc3d4c9a5e14c91
Du Song <freewizard@gmail.com>
2012-09-19 18:22:40 +0800

    code & doc

---
+++ GotoFolder.py
@@ -0,0 +1,94 @@
+import sublime, sublime_plugin
+import subprocess, os
+
+class GotoFolderCommand(sublime_plugin.ApplicationCommand):
+	def __init__(self):
+		self.app_path_mac = None
+		self.folder_list = None
+		self.cache = False
+
+	def get_sublime_path(self):
+		if sublime.platform() == 'osx':
+			if not self.app_path_mac:
+				from ctypes import cdll, byref, Structure, c_int, c_char_p, c_void_p
+				from ctypes.util import find_library
+				Foundation = cdll.LoadLibrary(find_library('Foundation'))
+				CFBundleGetMainBundle = Foundation.CFBundleGetMainBundle
+				CFBundleGetMainBundle.restype = c_void_p
+				bundle = CFBundleGetMainBundle()
+				CFBundleCopyBundleURL = Foundation.CFBundleCopyBundleURL
+				CFBundleCopyBundleURL.argtypes = [c_void_p]
+				CFBundleCopyBundleURL.restype = c_void_p
+				url = CFBundleCopyBundleURL(bundle)
+				CFURLCopyFileSystemPath = Foundation.CFURLCopyFileSystemPath
+				CFURLCopyFileSystemPath.argtypes = [c_void_p, c_int]
+				CFURLCopyFileSystemPath.restype = c_void_p
+				path = CFURLCopyFileSystemPath(url, c_int(0))
+				CFStringGetCStringPtr = Foundation.CFStringGetCStringPtr
+				CFStringGetCStringPtr.argtypes = [c_void_p, c_int]
+				CFStringGetCStringPtr.restype = c_char_p
+				self.app_path_mac = CFStringGetCStringPtr(path, 0)
+				CFRelease = Foundation.CFRelease
+				CFRelease.argtypes = [c_void_p]
+				CFRelease(path)
+				CFRelease(url)
+			return self.app_path_mac + '/Contents/SharedSupport/bin/subl'
+		elif sublime.platform() == 'linux':
+			return open('/proc/self/cmdline').read().split(chr(0))[0]
+		else:
+			return sys.executable
+
+	def sublime_command_line(self, args):
+		return
+		return subprocess.Popen(args)
+
+	def run(self):
+		if not sublime.active_window():
+			sublime.run_command("new_window")
+			self.use_active_window = True
+		else:
+			self.use_active_window = False
+		if not self.cache or not self.folder_list:
+			settings = sublime.load_settings('GotoFolder.sublime-settings')
+			self.cache = settings.get("cache", False);
+			root_folders = settings.get("root_folders", [{'folder': sublime.packages_path(), 'alias': 'Plugin'}])
+			self.folder_list = [];
+			self.folder_info = [];
+			folder_count = 0;
+			for root in root_folders:
+				if not root.has_key('alias'):
+					root['alias'] = ""
+				else:
+					root['alias'] += ":"
+				for f in os.listdir(root['folder']):
+					if f.startswith(".") or os.path.isfile(root['folder']+"/"+f):
+						continue
+					self.folder_list.append([root['alias']+f])
+					a = [root['folder']+"/"+f]
+					if root.has_key('extra'):
+						a += root['extra']
+					self.folder_info.append(a)
+					folder_count+=1
+			#print "GotoFolder Plugin:", folder_count, "folders added to the list"
+		sublime.active_window().show_quick_panel(self.folder_list, self.on_select)
+
+	def on_select(self, idx):
+		if idx < 0:
+			return
+		# disabled due to lack of sublime.focus_window API
+		#if self.find_and_focus_folder(folder_list[idx][1]): return
+		args = ["-n"] + self.folder_info[idx]
+		if self.use_active_window:
+			args[0] = "-a"
+		self.sublime_command_line(args)
+
+	def find_and_focus_folder(self, folder):
+		for w in sublime.windows(): 
+			for f in w.folders():
+				if f == folder:
+					sublime.focus_window(w)
+					#sublime.set_timeout(partial(ctypes.windll.user32.SetForegroundWindow, window.hwnd(), 0), 500)
+					return True
+		return False
+
+
--- README.md
+++ README.md
@@ -1,4 +1,37 @@
-SublimeGotoFolder
-=================
+## Summary
 
 SublimeGotoFolder plugin lists folders similar to the handy Goto Anything, and opens selected folder in a separate window by your command.
+
+## How to Use
+
+Customize with GotoFolder.sublime-settings, otherwise this plugin will start with default settings of scaning all Sublime Text 2 plugins.
+
+Press _Ctrl + P_ or click menu _Goto_ -> _Goto Folder_
+
+## Example Configuration
+create GotoFolder.sublime-settings with content like this:
+
+```
+{
+    "root_folders": [{
+    	"folder": "/Users/freewizard/Projects/as3", //required
+    	"extra": ["/Users/freewizard/Projects/as3/common"], //optional
+    	"alias": "AS3" //optional
+    },{
+    	"folder": "/Users/freewizard/Projects/cocoa",
+    	"extra": ["/Users/freewizard/Projects/cocoa/common"],
+    	"alias": "iOS"
+    },{
+    	"folder": "/Users/freewizard/Projects/android",
+    	"extra": ["/Users/freewizard/Projects/android/common"],
+    	"alias": "Android"
+    },
+    "cache": true //set cache to true to save scan time
+}
+```
+
+all sub-folders in as3/cocoa/android will be listed in Goto Folder; when pressing enter, the common folder will be opened together with the selected folder
+
+## License
+
+[MIT LICENSE](https://github.com/holtwick/xobjc/blob/master/LICENSE-MIT.txt)
